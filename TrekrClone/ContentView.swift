8d90edd9019be7d2a3513b730499eda25b059a43
LeoBrombillaAntunes <antunes.b.leonardo@gmail.com>
2022-06-18 08:23:23 -0300

    Initial Commit

---
+++ TrekrClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrekrClone
+//
+//  Created by Leonardo Antunes on 18/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrekrClone/TrekrCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrCloneApp.swift
+//  TrekrClone
+//
+//  Created by Leonardo Antunes on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
