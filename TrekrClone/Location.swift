b024b0d070e7f09e5bf42441dd898127dbad9daa
LeoBrombillaAntunes <antunes.b.leonardo@gmail.com>
2022-06-21 08:02:47 -0300

    Adding text to layout

---
+++ TrekrClone/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  TrekrClone
+//
+//  Created by Leonardo Antunes on 21/06/22.
+//
+
+import Foundation
+
+struct Location {
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let advisory: String
+}
